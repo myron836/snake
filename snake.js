d908616700a426a13f0925b39b790475db7932cd
Myron Langlais <myronlanglais@Myrons-MacBook-Pro.local>
2017-09-04 17:41:39 +0100

    Snake will now collect food and extend tail properly

--- snake.js
+++ snake.js
@@ -20,85 +20,75 @@ var up = 87;
 var down = 83;
 /* ########## */
 
+var head = snakeBody[snakeBody.length - 1];
+createPiece(head)
+
 window.onkeydown = function (e) {
+    //followBody();
+    var head = snakeBody[snakeBody.length - 1];
+    if(e.keyCode == right && head[1] < 11)
     {
-        if(e.keyCode == right && snakeBody[0][1] < 11)
-        {
-            snakeBody[0][1] += 1;
-            snakeHead.style.left = snakeBody[0][1] * cellSize + 'px';
-            console.log('Right 1');
-        } 
-        else if(e.keyCode == left && snakeBody[0][1] > 0) 
-        {
-            snakeBody[0][1] -= 1;
-            snakeHead.style.left = snakeBody[0][1] * cellSize + 'px';
-            console.log('Left 1');
-        }
-        else if(e.keyCode == up && snakeBody[0][0] > 0)
-        {
-            snakeBody[0][0] -= 1;
-            snakeHead.style.top = snakeBody[0][0] * cellSize + 'px';
-            console.log('Up 1');
-        }
-        else if(e.keyCode == down && snakeBody[0][0] < 11) 
-        {
-            snakeBody[0][0] += 1;
-            snakeHead.style.top = snakeBody[0][0] * cellSize + 'px';
-            console.log('Down 1');
-        }
-        console.log('Head Is At ' + snakeBody[0][0] + ' ' + snakeBody[0][1])
-        foodRandom();
-        followBody();
+        head[1] += 1;
+        console.log('Right 1');
+    } 
+    else if(e.keyCode == left && head[1] > 0) 
+    {
+        head[1] -= 1;
+        console.log('Left 1');
     }
+    else if(e.keyCode == up && head[0] > 0)
+    {
+        head[0] -= 1;
+        console.log('Up 1');
+    }
+    else if(e.keyCode == down && head[0] < 11) 
+    {
+        head[0] += 1;
+        console.log('Down 1');
+    }
+    console.log('Head Is At ' + head[0] + ' ' + head[1])
+    // game logic
+    foodRandom(head);
+    followBody(head);
+
+    // rendering
+    renderSnake(snakeBody);
 }
 
-function createPiece () {
+function createPiece (bodyPiece) {
     var snakeBodyNew = document.createElement("div");
     snakeBodyNew.classList.add("bodyPiece");
     document.getElementById("arena").appendChild(snakeBodyNew);
     var createNewArray = [0, 0, snakeBodyNew];
-    snakeBody.push(createNewArray);
-    console.log('NEW BODY PIECE ' + createNewArray[0] + ' ' + createNewArray[1]);
+    snakeBody.unshift(createNewArray);
     console.log('FOOD HAS SPAWNED ' + foodPosition[0] + ' ' + foodPosition[1]);
     
 }
 
-function foodRandom() {
-    if (snakeBody[0][0] === foodPosition[0] && snakeBody[0][1] === foodPosition[1]) { 
+function foodRandom(head) {
+    if (head[0] === foodPosition[0] && head[1] === foodPosition[1]) { 
         foodPosition[0] = Math.floor(Math.random() * cellCanvas);
         foodPosition[1] = Math.floor(Math.random() * cellCanvas);
         food.style.top = foodPosition[0] * cellSize + 'px';
         food.style.left = foodPosition[1] * cellSize + 'px';
-        createPiece()
+        createPiece(head)
     }     
 }
 
 /*var arrayLength = 1; arrayLength < snakeBody.length; arrayLength++*/
-function followBody() {
-    for (var arrayLength = 1; arrayLength < snakeBody.length; arrayLength++) {
-        var test = 1;
-        ++test;
-        //bodyPiece = snakeBody[0][1];
+
+function followBody(head) {
+    for (var index = 1; index < snakeBody.length; index++) {
         console.log('working?');
 
-        snakeBody[arrayLength][0] = snakeBody[0][0] -test;
-        snakeBody[arrayLength][1] = snakeBody[0][1] -test;
-        
-        /*var test3 = 0;
-        var newPos = snakeBody[arrayLength][0];
+        snakeBody[index-1][0] = snakeBody[index][0];
+        snakeBody[index-1][1] = snakeBody[index][1];
+    }
+}
 
-        var test = snakeBody[0][1];
-        var test2 = snakeBody[0][0];
-
-        snakeBody[arrayLength][0] = test2;
-        snakeBody[arrayLength][1] = test;
-
-        test3++;
-
-        test = test - test3;
-        test2 = test2 - test3;
-        */
-        //bodyPiece.style.top = bodyPiece * cellSize + 'px';
-        //bodyPiece.style.left = bodyPiece * cellSize + 'px';
+function renderSnake(snakeBody) {
+    for (var index = 0; index < snakeBody.length; index++) {
+        snakeBody[index][2].style.left = snakeBody[index][1]* cellSize + 'px';
+        snakeBody[index][2].style.top = snakeBody[index][0]* cellSize + 'px'; 
     }
 }
